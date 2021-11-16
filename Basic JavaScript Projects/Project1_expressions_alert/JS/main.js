07178d465fb5e2d75e6701352fbdcdcc49d73ac2
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2021-11-16 16:17:24 -0500

    Update main.js
    add

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,21 +1,26 @@
-alert("Hello, world!!!");
-document.write("Hello, world!");
+window.alert("Hello, world!!!");  //dropdown window with message
+document.write("Hello, world!");  //display Hello,world! in browser
 
-var A = "This is a string!";
+var A = "This is a string!";     //defining a string, setting variable 
 
-document.write(A);
+document.write(A);          //displaying variable A in browser
 
-document.write("I don't like JavaScript too much. \ I need a nap. ")
+document.write("I'm hungry. \ I need a nap. ")  //display variables and concatenate variables
 
-document.write("\"To be the squirrel," + " you must become the squirrel.\"")
+document.write("\"To be the squirrel," + " you must become the squirrel.\"")  //
 
-var person = "John Doe", Age = "34", Height = "6ft", Weight = "190lbs."
+var person = "John Doe", Age = "34", Height = "6ft", Weight = "190lbs."  //assigning variables their definitions
 
-var person = person.fontcolor("blue");
-var Age = Age.fontcolor("red");
-var Height = Height.fontcolor("green");
-var Weight = Weight.fontcolor("purple");
-document.write(person);
-document.write(Age);
-document.write(Height)
-document.write(Weight)
+var person = person.fontcolor("blue");  //assigning variable Person a font color
+var Age = Age.fontcolor("red");         //assigning variable Age a font color
+var Height = Height.fontcolor("green");  //assigning variable Height a font color
+var Weight = Weight.fontcolor("purple");  //assigning variable Weight a font color
+document.write(person);     //display variable defintion in browser
+document.write(Age);        //display variable defintion in browser
+document.write(Height);     //display variable defintion in browser
+document.write(Weight);     //display variable defintion in browser
+
+document.write(4 + 4);  //displaying an expression in browser
+
+var B = "rock" + "paper" + "scissors"       //assigning a concatenated string value to a variable
+document.write(B)                           //displaying variable B in browser
