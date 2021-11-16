f1d0f6cc1b4a53747fe1798a197116f9cf803dd4
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2021-11-16 14:32:40 -0500

    add
    tutorials etc

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -4,3 +4,18 @@ document.write("Hello, world!");
 var A = "This is a string!";
 
 document.write(A);
+
+document.write("I don't like JavaScript too much. \ I need a nap. ")
+
+document.write("\"To be the squirrel," + " you must become the squirrel.\"")
+
+var person = "John Doe", Age = "34", Height = "6ft", Weight = "190lbs."
+
+var person = person.fontcolor("blue");
+var Age = Age.fontcolor("red");
+var Height = Height.fontcolor("green");
+var Weight = Weight.fontcolor("purple");
+document.write(person);
+document.write(Age);
+document.write(Height)
+document.write(Weight)
