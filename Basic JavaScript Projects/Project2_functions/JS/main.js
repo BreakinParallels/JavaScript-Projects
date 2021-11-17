9e893f1d5da1cfe3026e7b7a9a6b457ea22b7409
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2021-11-16 21:04:03 -0500

    Update main.js
    add

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,11 +1,13 @@
+
+//Calls on function to assign variables like font color and string
 function JavaScript_Practice() {
     var str = "The time is?";
     var result = str.fontcolor("orange");
     document.getElementById("Orange_Text").innerHTML = result;
 }
-
+//assigns variable (sentence) and uses operator to concatenate a string
 function JavaScript_Practice() {
     var sentence = "I am not learning";
-    sentence += " a lot from this class!";
+    sentence += " a lot from this.";
     document.getElementById("Concatenate").innerHTML = sentence;
 }
