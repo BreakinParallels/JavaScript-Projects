6a0de46071c95e3c9df55cb16e40130edc37e3d3
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2021-11-22 12:36:07 -0500

    add changes

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -4,10 +4,11 @@
 //Global Scope Variable example:
   var X = 25;
   function Add_numbers_1() {
-      document.write(20 + X + "<br>");
+      document.getElementById("Global Scope").innerHTML = 20 + X;
   }
+
   function Add_numbers_2() {
-      document.write(X + 100);
+      document.getElementById("Global Scope 2").innerHTML = 100 + X;
   }
   Add_numbers_1();
   Add_numbers_2();
@@ -15,10 +16,10 @@
   //Local Variable Example:
   function Add_numbers_3() {
       var X = 17;
-    document.write(20 + X + "<br>");
+    document.getElementById("Local Variable").innerHTML = 20 + X;
 }
 function Add_numbers_4() {
-    document.write(X + 100);
+    document.getElementById("Local Variable 2").innerHTML = X + 100;
 }
 Add_numbers_3();
 Add_numbers_4();
@@ -28,7 +29,7 @@ Add_numbers_4();
 //Using Console log method to detect errors:
 function Add_numbers_5() {
     var X = 15;
-    console.log(15 + X);
+    console.log(15 + Y);
 }
 function Add_numbers_6() {
     console.log(X + 100);
@@ -61,7 +62,7 @@ function Age_Function() {
         Vote = "You are NOT old enough to vote!";
     }
     else {
-        Vote = "You ARE old enough to vote!";
+        Vote = "You are old enough to vote!";
     }
     document.getElementById("How_old_are_you?").innerHTML = Vote;
 }
